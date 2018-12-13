7220b83c2464c0de7f1bdaf7306faee4770921d3
Mikołaj Śliwiński <mikolaj.sliwinski@garneczki.pl>
2018-12-13 14:02:20 +0100

    deploy

---
+++ precache-manifest.ae53e2b7d82e153931e06de4f952e3d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa-test/robots.txt"
+  },
+  {
+    "revision": "70b838040514ffa0f67d",
+    "url": "/vue-pwa-test/js/chunk-vendors.1283f017.js"
+  },
+  {
+    "revision": "557d2f9d0b6858430fa7",
+    "url": "/vue-pwa-test/js/app.0bec021a.js"
+  },
+  {
+    "revision": "8871b0dd4d9fc9649819",
+    "url": "/vue-pwa-test/js/about.a956700f.js"
+  },
+  {
+    "revision": "1d4bb2cc52377b0f594f6c5ea3b2823e",
+    "url": "/vue-pwa-test/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa-test/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "557d2f9d0b6858430fa7",
+    "url": "/vue-pwa-test/css/app.80d62a31.css"
+  }
+];
