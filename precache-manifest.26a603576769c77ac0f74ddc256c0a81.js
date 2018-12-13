a2cb6be2f18a9ea528d9636886a90b63534b260d
Mikołaj Śliwiński <mikolaj.sliwinski@garneczki.pl>
2018-12-13 13:45:12 +0100

    Initial build

---
+++ precache-manifest.26a603576769c77ac0f74ddc256c0a81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "70b838040514ffa0f67d",
+    "url": "/js/chunk-vendors.1283f017.js"
+  },
+  {
+    "revision": "3d8bbc731ca42d4244ed",
+    "url": "/js/app.134e5084.js"
+  },
+  {
+    "revision": "8871b0dd4d9fc9649819",
+    "url": "/js/about.a956700f.js"
+  },
+  {
+    "revision": "d9d9408c72d15d877116adfcb6db2263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3d8bbc731ca42d4244ed",
+    "url": "/css/app.80d62a31.css"
+  }
+];
